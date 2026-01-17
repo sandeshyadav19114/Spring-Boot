b3618adef30a17d211453976c0d157f570860694
Sandesh Yadav <sandeshyadav19114@gmail.com>
2026-01-17 18:23:23 +0530

    Update UserService.java

--- src/main/java/com/api/exapmle/service/UserService.java
+++ src/main/java/com/api/exapmle/service/UserService.java
@@ -5,6 +5,10 @@ import com.api.exapmle.entity.User;
 import com.api.exapmle.exception.ResourceNotFound;
 import com.api.exapmle.repository.UserRepository;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -35,8 +39,7 @@ public class UserService {
     }
 
     public List<User> getALLEmployees() {
-        List<User> users = userRepository.findAll();
-        return users;
+        return userRepository.findAll();
     }
     public User getRegistrationById(long id) {
 
@@ -45,6 +48,54 @@ public class UserService {
         );
         return user;
     }
- 
-}
+
+// ********************************************** Pagination & Sorting in Spring boot - JPARepository  **********************
+    public List<User> getRegistrations(int pageNo, int pageSize) {
+
+            Pageable pageable = PageRequest.of(pageNo, pageSize);
+            Page<User> page = userRepository.findAll(pageable);
+            List<User> users = page.getContent();
+            return users;
+
+        }
+
+//  *************************************************  Sorting  ********************************************************************
+    
+    public List<User> getRegistrations(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = null;
+
+
+//Sort sort = null;
+//		sort = sortDir.equalsIgnoreCase("asc")?Sort.by(Direction.ASC,sortBy):Sort.by(Direction.DESC,sortBy);
+
+        if(sortDir.equalsIgnoreCase("asc")) {
+            sort = Sort.by(Sort.Direction.ASC, sortBy);
+        }else {
+            sort = Sort.by(Sort.Direction.DESC, sortBy);
+        }
+
+        Pageable pageable = PageRequest.of(pageNo, pageSize,sort);
+        Page<User> page = userRepository.findAll(pageable);
+        List<User> users = page.getContent();
+
+            sort = sortDir.equalsIgnoreCase("asc")?Sort.by(Sort.Direction.ASC, sortBy):Sort.by(Sort.Direction.DESC, sortBy);
+
+            pageable = PageRequest.of(pageNo, pageSize, sort);
+            page = userRepository.findAll(pageable);
+            users = page.getContent();
+
+            System.out.println(page.getNumber());
+            System.out.println(page.getSize());
+            System.out.println(page.getTotalPages());
+            System.out.println(page.getTotalElements());
+            System.out.println(pageable.getPageNumber());
+            System.out.println(pageable.getPageSize());
+
+            return users;
+
+        }
+
+
+    }
+
 
